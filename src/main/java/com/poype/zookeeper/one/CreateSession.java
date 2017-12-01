9f6075e37052755128db9cedf9859f66cc1f34b0
poype <253186926@qq.com>
2017-12-01 11:00:30 +0800

    上次提交的代码是看不到watcher中的输出的，因为还没等到与zk服务器建立好连接，main就 退出了。 为了能够看到watcher中的输出代码，在main函数中添加一行sleep代码。

--- src/main/java/com/poype/zookeeper/one/CreateSession.java
+++ src/main/java/com/poype/zookeeper/one/CreateSession.java
@@ -3,11 +3,13 @@ package com.poype.zookeeper.one;
 import org.apache.zookeeper.ZooKeeper;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class CreateSession {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         ZooKeeper zooKeeper = new ZooKeeper("192.168.1.108:2181", 5000,
                 new CreateSessionWatcher());
+        TimeUnit.MILLISECONDS.sleep(Integer.MAX_VALUE);
     }
 }
