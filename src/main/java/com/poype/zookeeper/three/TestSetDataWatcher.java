e711fb4653ea96f78b281260606692d02da8eb79
poype <253186926@qq.com>
2017-12-03 10:47:40 +0800

    异步设置一个节点的数据值

--- src/main/java/com/poype/zookeeper/three/TestSetDataWatcher.java
+++ src/main/java/com/poype/zookeeper/three/TestSetDataWatcher.java
@@ -1,9 +1,6 @@
 package com.poype.zookeeper.three;
 
-import org.apache.zookeeper.KeeperException;
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
 import org.apache.zookeeper.data.Stat;
 
 public class TestSetDataWatcher implements Watcher {
@@ -13,13 +10,7 @@ public class TestSetDataWatcher implements Watcher {
     public void process(WatchedEvent watchedEvent) {
         if (watchedEvent.getState() == Event.KeeperState.SyncConnected) {
             if(watchedEvent.getType() == Event.EventType.None && null == watchedEvent.getPath()) {
-                try {
-                    setDataSync();
-                } catch (KeeperException e) {
-                    e.printStackTrace();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                setDataAsync();
             }
         }
     }
@@ -31,5 +22,16 @@ public class TestSetDataWatcher implements Watcher {
     private void setDataSync() throws KeeperException, InterruptedException {
         Stat stat = zooKeeper.setData("/poype_node2", "poype5211314".getBytes(), 1);
         System.out.println(stat);
+    }
+
+    private void setDataAsync() {
+        zooKeeper.setData("/poype_node2", "poype5211314".getBytes(), 3, new AsyncCallback.StatCallback() {
+            public void processResult(int resultCode, String path, Object ctx, Stat stat) {
+                System.out.println(resultCode);
+                System.out.println(path);
+                System.out.println(ctx);
+                System.out.println(stat.getVersion()); // 获取数据节点版本号
+            }
+        }, "异步设置一个节点的数据");
     }
 }
