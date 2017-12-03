3a97f2208560ac9fa26b5bde551e6184739ba4de
poype <253186926@qq.com>
2017-12-03 09:59:24 +0800

    异步查看一个节点是否存在

--- src/main/java/com/poype/zookeeper/two/TestExistWatcher.java
+++ src/main/java/com/poype/zookeeper/two/TestExistWatcher.java
@@ -1,9 +1,6 @@
 package com.poype.zookeeper.two;
 
-import org.apache.zookeeper.KeeperException;
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
 import org.apache.zookeeper.data.Stat;
 
 public class TestExistWatcher implements Watcher {
@@ -19,40 +16,16 @@ public class TestExistWatcher implements Watcher {
     public void process(WatchedEvent watchedEvent) {
         if (watchedEvent.getState() == Event.KeeperState.SyncConnected) {
             if(watchedEvent.getType() == Event.EventType.None && null == watchedEvent.getPath()) {
-                try {
-                    existSync();
-                } catch (KeeperException e) {
-                    e.printStackTrace();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                existAsync();
             } else if(watchedEvent.getType() == Event.EventType.NodeCreated) { //节点创建
                 System.out.println("监控到创建了一个节点");
-                try {
-                    existSync();
-                } catch (KeeperException e) {
-                    e.printStackTrace();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                existAsync();
             } else if(watchedEvent.getType() == Event.EventType.NodeDataChanged) {
                 System.out.println("监控到了一个节点变化");
-                try {
-                    existSync();
-                } catch (KeeperException e) {
-                    e.printStackTrace();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                existAsync();
             } else if(watchedEvent.getType() == Event.EventType.NodeDeleted) {
                 System.out.println("监控到了一个节点被删除");
-                try {
-                    existSync();
-                } catch (KeeperException e) {
-                    e.printStackTrace();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                existAsync();
             }
         }
     }
@@ -60,5 +33,16 @@ public class TestExistWatcher implements Watcher {
     private void existSync() throws KeeperException, InterruptedException {
         Stat stat = zooKeeper.exists("/poype_node2", true);
         System.out.println(stat);
+    }
+
+    private void existAsync() {
+        zooKeeper.exists("/poype_node2", true, new AsyncCallback.StatCallback() {
+            public void processResult(int resultCode, String path, Object ctx, Stat stat) {
+                System.out.println(resultCode);
+                System.out.println(path);
+                System.out.println(ctx);
+                System.out.println(stat);
+            }
+        }, "异步查看一个节点是否存在");
     }
 }
